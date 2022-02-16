7b627017c888af36402bb5eb60e0ddf35c3fb2bf
Luisisaza964 <61132151+Luisisaza964@users.noreply.github.com>
2022-02-15 19:49:58 -0500

    Update Index.py

--- testing/Index.py
+++ testing/Index.py
@@ -1,9 +1,39 @@
-a = 3
+class Animales(object):
+    nombre = ""
+    genero = ""
+    edad = ""
+    raza = ""
+    comida=False
 
-b = 5
+    def __init__(self):
+        pass 
+    
 
-c = a + b
+    def comer(self,comida):
+        comida=True
+        
+    def nombre(self,_nombre,accion,sonidoemitido):
+        self._nombre=_nombre
+        self.accion=accion
+        self.sonidoemitido=sonidoemitido
 
-print(c)
+class Terrestres(Animales):
+    nombre = ""
+    genero = ""
+    edad = ""
+    raza = ""
+    pelaje = ""
+    limpieza = ""
+    agresividad = ""
+    vacunas = ""
 
+    def __init__(self):
+        pass
 
+    
+    def nombre(self,nombre,accion,sonidoemitido,metodoalimentacion,desplazamiento):
+        self.nombre=nombre
+        self.accion=accion
+        self.sonidoemitido=sonidoemitido
+        self.metodoalimentacion=metodoalimentacion
+        self.desplazamiento=desplazamiento
