8906e615dfb5fac2b73ed03a54406e961ee5d1dd
Luis Isaza <luisisaza964@yahoo.com>
2022-02-19 19:31:28 -0500

    Agrega Clase Acuaticos

---
+++ testing/Acuaticos.py
@@ -0,0 +1,47 @@
+from turtle import color
+
+
+class Animales(object):
+    nombre = ""
+    genero = ""
+    edad = ""
+    raza = ""
+    color=""
+    habitad=""
+    temperatura=""
+    alimentacion=""
+    
+    def __init__(self):
+        pass 
+    
+    def metodos_acuaticos(self,comer,sonidoemitido,accion,velocidad,metodocaza,metododefensa):
+        self.comer=comer
+        self.sonidoemitido=sonidoemitido
+        self.velocidad=velocidad
+        self.velocidad=metodocaza
+        self.velocidad=metododefensa
+
+    def comer(self, comidaacuaticos):
+        comidaacuaticos=input("Ingrese Alimento: ")
+        return comidaacuaticos
+
+    def sonidoemitido(self, sonidoemitidoacuaticos):
+        sonidoemitidoacuaticos=input("Como se Muve el animal Terrestre: ")
+        return sonidoemitidoacuaticos
+
+    def accion(self, accionacuaticos):
+        accionacuaticos=input("Con que juega el animal Terrestre: ")
+        return accionacuaticos
+
+    def velocidad(self, velocidadacuaticos):
+        accionacuaticos=input("Con que juega el animal Terrestre: ")
+        return velocidadacuaticos
+
+    def metodocaza(self, metodocazaacuaticos):
+        metodocazaacuaticos=input("Con que juega el animal Terrestre: ")
+        return metodocazaacuaticos
+
+    def metodocaza(self, metododefensaacuaticos):
+        metododefensaacuaticos=input("Con que juega el animal Terrestre: ")
+        return metododefensaacuaticos
+    
