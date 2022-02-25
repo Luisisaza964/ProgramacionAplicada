c1354486a0b874efb59da69fa8fc8bfb7472b09b
lauraximenalopezc <lauraximenalopezc@gmail.com>
2022-02-24 19:40:23 -0500

    crear clase animales

--- testing/Acuaticos.py
+++ testing/Acuaticos.py
@@ -1,7 +1,7 @@
 #from turtle import color
 
 
-class AnimalesAcuaticos(object):
+class AnimalesAcuaticos(metodos_animales):
     nombre = ""
     genero = ""
     edad = ""
@@ -46,102 +46,102 @@ class AnimalesAcuaticos(object):
         return metododefensaacuaticos
     
 
-class AnimalesAcuaticosDelfin(object):
-    nombre = ""
-    genero = ""
-    edad = ""
-    raza = ""
-    color=""
-    habitad=""
-    temperatura=""
-    alimentacion=""
-    
-    def __init__(self):
-        pass 
-    
-    def metodos_acuaticosdelfin(self,comer,sonidoemitido,accion,velocidad,metodocaza,metododefensa,socializacion):
-        self.comer=comer
-        self.sonidoemitido=sonidoemitido
-        self.accion=accion
-        self.velocidad=velocidad
-        self.metodocaza=metodocaza
-        self.metododefensa=metododefensa
-        self.socializacion=socializacion
+    class AnimalesAcuaticosDelfin(metodos_acuaticos):
+        nombre = ""
+        genero = ""
+        edad = ""
+        raza = ""
+        color=""
+        habitad=""
+        temperatura=""
+        alimentacion=""
+        
+        def __init__(self):
+            pass 
+        
+        def metodos_acuaticosdelfin(self,comer,sonidoemitido,accion,velocidad,metodocaza,metododefensa,socializacion):
+            self.comer=comer
+            self.sonidoemitido=sonidoemitido
+            self.accion=accion
+            self.velocidad=velocidad
+            self.metodocaza=metodocaza
+            self.metododefensa=metododefensa
+            self.socializacion=socializacion
 
-    def comer(self, comidaacuaticosdelfin):
-        comidaacuaticosdelfin=input("Ingrese Alimento: ")
-        return comidaacuaticosdelfin
+        def comer(self, comidaacuaticosdelfin):
+            comidaacuaticosdelfin=input("Ingrese Alimento: ")
+            return comidaacuaticosdelfin
 
-    def sonidoemitido(self, sonidoemitidoacuaticosdelfin):
-        sonidoemitidoacuaticosdelfin=input("Sonido Emitido del Delfin: ")
-        return sonidoemitidoacuaticosdelfin
+        def sonidoemitido(self, sonidoemitidoacuaticosdelfin):
+            sonidoemitidoacuaticosdelfin=input("Sonido Emitido del Delfin: ")
+            return sonidoemitidoacuaticosdelfin
 
-    def accion(self, accionacuaticosdelfin):
-        accionacuaticosdelfin=input("Accion del Delfin: ")
-        return accionacuaticosdelfin
+        def accion(self, accionacuaticosdelfin):
+            accionacuaticosdelfin=input("Accion del Delfin: ")
+            return accionacuaticosdelfin
 
-    def velocidad(self, velocidadacuaticosdelfin):
-        accionacuaticosdelfin=input("Cual es la velocidad del Delfin: ")
-        return velocidadacuaticosdelfin
+        def velocidad(self, velocidadacuaticosdelfin):
+            accionacuaticosdelfin=input("Cual es la velocidad del Delfin: ")
+            return velocidadacuaticosdelfin
 
-    def metodocaza(self, metodocazaacuaticosdelfin):
-        metodocazaacuaticosdelfin=input("Como caza el delfin: ")
-        return metodocazaacuaticosdelfin
+        def metodocaza(self, metodocazaacuaticosdelfin):
+            metodocazaacuaticosdelfin=input("Como caza el delfin: ")
+            return metodocazaacuaticosdelfin
 
-    def metododefensa(self, metododefensaacuaticosdelfin):
-        metododefensaacuaticosdelfin=input("Como se defienden los delfines: ")
-        return metododefensaacuaticosdelfin
+        def metododefensa(self, metododefensaacuaticosdelfin):
+            metododefensaacuaticosdelfin=input("Como se defienden los delfines: ")
+            return metododefensaacuaticosdelfin
 
-    def socializacion(self, metododefensaacuaticosdelfin):
-        socializacion=input("Que tan frecuente socializan los Delfines: ")
-        return socializacion
+        def socializacion(self, metododefensaacuaticosdelfin):
+            socializacion=input("Que tan frecuente socializan los Delfines: ")
+            return socializacion
 
-class AnimalesAcuaticosTiburon(object):
-    nombre = ""
-    genero = ""
-    edad = ""
-    raza = ""
-    color=""
-    habitad=""
-    temperatura=""
-    alimentacion=""
-    
-    def __init__(self):
-        pass 
-    
-    def metodos_acuaticostiburon(self,comer,sonidoemitido,accion,velocidad,metodocaza,metododefensa,socializacion):
-        self.comer=comer
-        self.sonidoemitido=sonidoemitido
-        self.accion=accion
-        self.velocidad=velocidad
-        self.metodocaza=metodocaza
-        self.metododefensa=metododefensa
-        self.socializacion=socializacion
+    class AnimalesAcuaticosTiburon(metodos_acuaticos):
+        nombre = ""
+        genero = ""
+        edad = ""
+        raza = ""
+        color=""
+        habitad=""
+        temperatura=""
+        alimentacion=""
+        
+        def __init__(self):
+            pass 
+        
+        def metodos_acuaticostiburon(self,comer,sonidoemitido,accion,velocidad,metodocaza,metododefensa,socializacion):
+            self.comer=comer
+            self.sonidoemitido=sonidoemitido
+            self.accion=accion
+            self.velocidad=velocidad
+            self.metodocaza=metodocaza
+            self.metododefensa=metododefensa
+            self.socializacion=socializacion
 
-    def comer(self, comidaacuaticostiburon):
-        comidaacuaticostiburon=input("Ingrese Alimento: ")
-        return comidaacuaticostiburon
+        def comer(self, comidaacuaticostiburon):
+            comidaacuaticostiburon=input("Ingrese Alimento: ")
+            return comidaacuaticostiburon
 
-    def sonidoemitido(self, sonidoemitidoacuaticostiburon):
-        sonidoemitidoacuaticostiburon=input("Sonido Emitido del Tiburon: ")
-        return sonidoemitidoacuaticostiburon
+        def sonidoemitido(self, sonidoemitidoacuaticostiburon):
+            sonidoemitidoacuaticostiburon=input("Sonido Emitido del Tiburon: ")
+            return sonidoemitidoacuaticostiburon
 
-    def accion(self, accionacuaticostiburon):
-        accionacuaticostiburon=input("Accion del Tiburon: ")
-        return accionacuaticostiburon
+        def accion(self, accionacuaticostiburon):
+            accionacuaticostiburon=input("Accion del Tiburon: ")
+            return accionacuaticostiburon
 
-    def velocidad(self, velocidadacuaticostiburon):
-        accionacuaticostiburon=input("Cual es la velocidad del Tiburon: ")
-        return velocidadacuaticostiburon
+        def velocidad(self, velocidadacuaticostiburon):
+            accionacuaticostiburon=input("Cual es la velocidad del Tiburon: ")
+            return velocidadacuaticostiburon
 
-    def metodocaza(self, metodocazaacuaticostiburon):
-        metodocazaacuaticostiburon=input("Como caza el tiburon: ")
-        return metodocazaacuaticostiburon
+        def metodocaza(self, metodocazaacuaticostiburon):
+            metodocazaacuaticostiburon=input("Como caza el tiburon: ")
+            return metodocazaacuaticostiburon
 
-    def metododefensa(self, metododefensaacuaticostiburon):
-        metododefensaacuaticostiburon=input("Como se defienden los tiburones: ")
-        return metododefensaacuaticostiburon
+        def metododefensa(self, metododefensaacuaticostiburon):
+            metododefensaacuaticostiburon=input("Como se defienden los tiburones: ")
+            return metododefensaacuaticostiburon
 
-    def socializacion(self, metododefensaacuaticostiburon):
-        socializacion=input("Que tan frecuente socializan los Tiburones: ")
-        return socializacion
+        def socializacion(self, metododefensaacuaticostiburon):
+            socializacion=input("Que tan frecuente socializan los Tiburones: ")
+            return socializacion
--- testing/Terrestres.py
+++ testing/Terrestres.py
@@ -1,4 +1,4 @@
-class AnimalesTerrestre(object):
+class AnimalesTerrestre(metodos_animales):
     nombre = ""
     genero = ""
     edad = ""
@@ -42,5 +42,106 @@ class AnimalesTerrestre(object):
     def metododedivercion(self, metododedivercionterrestre):
         metododedivercionterrestre=input("como se divierte el animal terrestre: ")
         return metododedivercionterrestre
+
+
+    class AnimalesTerrestresLeon(metodos_terrestres):
+        nombre = ""
+        genero = ""
+        edad = ""
+        limpieza = ""
+        agrecividad = ""
         
 
+        def __init__(self):
+            pass 
+
+
+        def metodos_terrestreleon(self,comer,sonidoemitido,metododedivercion,metododealimentacion,accion,rapidez,metododecaza):
+            self.comer=comer
+            self.sonidoemitido=sonidoemitido
+            self.metododedivercion=metododedivercion
+            self.metododealimentacion=metododealimentacion
+            self.accion=accion
+            self.rapidez=rapidez
+            self.metododecaza=metododecaza
+
+
+        def comer(self, comidaterrestreleon):
+            comidaterrestreleon=input("Ingrese comida: ")
+            return comidaterrestreleon
+
+        def sonidoemitido(self, sonidoterrestreleon):
+            sonidoterrestreleon=input("ingresar sonido: ")
+            return sonidoterrestreleon
+
+        def metododedivercion(self, juegoterrestreleon):
+            juegoterrestreleon=input("ingresar juego: ")
+            return juegoterrestreleon
+
+        def metododealimentacion(self, alimentacionterrestreleon):
+            alimentacionterrestreleon=input("ingresar alimento: ")
+            return alimentacionterrestreleon
+
+        def accion(self, accionterrestreleon):
+            accionterrestreleon=input("ingresar accion: ")
+            return accionterrestreleon
+
+        def rapidez(self, rapidezterrestreleon):
+            rapidezterrestreleon=input("ingresar rapidez: ")
+            return rapidezterrestreleon
+
+        def metododecaza(self, metododecazaterrestreleon):
+            metododecazaterrestreleon=input("ingresar metodo: ")
+            return metododecazaterrestreleon
+
+
+    class AnimalesTerrestresMico(metodos_terrestres):
+        nombre = ""
+        genero = ""
+        edad = ""
+        raza = ""
+        limpieza = ""
+        pelaje = ""
+        agresividad = ""
+        
+
+        def __init__(self):
+            pass 
+
+
+        def metodos_terrestresmico(self,comer,metododedesplazamiento,sonidoemitido,accion,metododedivercion,metododealimentacion,metododedefensa):
+            self.comer=comer
+            self.metododedesplazamiento=metododedesplazamiento
+            self.sonidoemitido=sonidoemitido
+            self.accion=accion
+            self.metododedivercion=metododedivercion
+            self.metododealimentacion=metododealimentacion
+            self.metododedefensa=metododedefensa
+
+
+        def comer(self, comidaterrestremico):
+            comidaterrestremico=input("Ingrese comida: ")
+            return comidaterrestremico
+
+        def metododedesplazamiento(self, desplazamientoterrestremico):
+            desplazamientoterrestremico=input("ingresar metodo de despazamiento: ")
+            return desplazamientoterrestremico
+
+        def sonidoemitido(self, sonidoterrestremico):
+            sonidoterrestremico=input("ingresar sonido: ")
+            return sonidoterrestremico
+
+        def accion(self, accionterrestremico):
+            accionterrestremico=input("ingresar accion: ")
+            return accionterrestremico
+
+        def metododedivercion(self, juegoterrestremico):
+            juegoterrestremico=input("ingresar juego:")
+            return juegoterrestremico
+
+        def metododealimentacion(self, alimentacionterrestremico):
+            alimentacionterrestremico=input("ingresar alimentacion: ")
+
+        def metododedefensa(self, defensaterrestremico):
+            defensaterrestremico=input("ingresar metodo de defensa")
+            return defensaterrestremico
