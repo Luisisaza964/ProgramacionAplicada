a488038c5812b4dcce6f67cdd39ffae56e590137
lauraximenalopezc <lauraximenalopezc@gmail.com>
2022-02-24 19:25:13 -0500

    modificar acuaticos y eliminar delfin y tiburon

--- testing/Acuaticos.py
+++ testing/Acuaticos.py
@@ -45,3 +45,103 @@ class AnimalesAcuaticos(object):
         metododefensaacuaticos=input("Con que juega el animal Acuatico: ")
         return metododefensaacuaticos
     
+
+class AnimalesAcuaticosDelfin(object):
+    nombre = ""
+    genero = ""
+    edad = ""
+    raza = ""
+    color=""
+    habitad=""
+    temperatura=""
+    alimentacion=""
+    
+    def __init__(self):
+        pass 
+    
+    def metodos_acuaticosdelfin(self,comer,sonidoemitido,accion,velocidad,metodocaza,metododefensa,socializacion):
+        self.comer=comer
+        self.sonidoemitido=sonidoemitido
+        self.accion=accion
+        self.velocidad=velocidad
+        self.metodocaza=metodocaza
+        self.metododefensa=metododefensa
+        self.socializacion=socializacion
+
+    def comer(self, comidaacuaticosdelfin):
+        comidaacuaticosdelfin=input("Ingrese Alimento: ")
+        return comidaacuaticosdelfin
+
+    def sonidoemitido(self, sonidoemitidoacuaticosdelfin):
+        sonidoemitidoacuaticosdelfin=input("Sonido Emitido del Delfin: ")
+        return sonidoemitidoacuaticosdelfin
+
+    def accion(self, accionacuaticosdelfin):
+        accionacuaticosdelfin=input("Accion del Delfin: ")
+        return accionacuaticosdelfin
+
+    def velocidad(self, velocidadacuaticosdelfin):
+        accionacuaticosdelfin=input("Cual es la velocidad del Delfin: ")
+        return velocidadacuaticosdelfin
+
+    def metodocaza(self, metodocazaacuaticosdelfin):
+        metodocazaacuaticosdelfin=input("Como caza el delfin: ")
+        return metodocazaacuaticosdelfin
+
+    def metododefensa(self, metododefensaacuaticosdelfin):
+        metododefensaacuaticosdelfin=input("Como se defienden los delfines: ")
+        return metododefensaacuaticosdelfin
+
+    def socializacion(self, metododefensaacuaticosdelfin):
+        socializacion=input("Que tan frecuente socializan los Delfines: ")
+        return socializacion
+
+class AnimalesAcuaticosTiburon(object):
+    nombre = ""
+    genero = ""
+    edad = ""
+    raza = ""
+    color=""
+    habitad=""
+    temperatura=""
+    alimentacion=""
+    
+    def __init__(self):
+        pass 
+    
+    def metodos_acuaticostiburon(self,comer,sonidoemitido,accion,velocidad,metodocaza,metododefensa,socializacion):
+        self.comer=comer
+        self.sonidoemitido=sonidoemitido
+        self.accion=accion
+        self.velocidad=velocidad
+        self.metodocaza=metodocaza
+        self.metododefensa=metododefensa
+        self.socializacion=socializacion
+
+    def comer(self, comidaacuaticostiburon):
+        comidaacuaticostiburon=input("Ingrese Alimento: ")
+        return comidaacuaticostiburon
+
+    def sonidoemitido(self, sonidoemitidoacuaticostiburon):
+        sonidoemitidoacuaticostiburon=input("Sonido Emitido del Tiburon: ")
+        return sonidoemitidoacuaticostiburon
+
+    def accion(self, accionacuaticostiburon):
+        accionacuaticostiburon=input("Accion del Tiburon: ")
+        return accionacuaticostiburon
+
+    def velocidad(self, velocidadacuaticostiburon):
+        accionacuaticostiburon=input("Cual es la velocidad del Tiburon: ")
+        return velocidadacuaticostiburon
+
+    def metodocaza(self, metodocazaacuaticostiburon):
+        metodocazaacuaticostiburon=input("Como caza el tiburon: ")
+        return metodocazaacuaticostiburon
+
+    def metododefensa(self, metododefensaacuaticostiburon):
+        metododefensaacuaticostiburon=input("Como se defienden los tiburones: ")
+        return metododefensaacuaticostiburon
+
+    def socializacion(self, metododefensaacuaticostiburon):
+        socializacion=input("Que tan frecuente socializan los Tiburones: ")
+        return socializacion
